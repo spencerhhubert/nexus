bbe3b589552ad55746b97793fc66f59fe332b7ca
spencer <spencerhhubert@gmail.com>
2021-06-18 21:30:24 -0700

    working for rendering over weekend

--- tools/image_from_3d_gen.py
+++ tools/image_from_3d_gen.py
@@ -8,6 +8,9 @@ from datetime import datetime
 def deg_to_radians(deg):
     return (deg / 360) * 2 * math.pi
 
+def random_radian():
+    return deg_to_radians(random() * 360)
+
 def timestamp():
     current_moment = datetime.now()
     timestamp = math.floor(datetime.timestamp(current_moment))
@@ -18,59 +21,65 @@ def deselect_all():
         bpy.ops.object.mode_set(mode='OBJECT')
     bpy.ops.object.select_all(action='DESELECT')
 
-#delete everything
-if bpy.context.object.mode == 'EDIT':
-    bpy.ops.object.mode_set(mode='OBJECT')
-bpy.ops.object.select_all(action="SELECT")
-bpy.ops.object.delete()
+def gen_pics(model, iterations, inPath, outPath, res):
+    seed()
+    #delete everything
+    if bpy.context.object.mode == 'EDIT':
+        bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.ops.object.select_all(action="SELECT")
+    bpy.ops.object.delete()
 
-model_path = "/home/spencer/ldraw/parts/"
-render_path = "/home/spencer/Documents/GitHub/nexus/tools/renders/"
+    path = os.path.join(inPath, model)
+    bpy.ops.import_scene.importldraw(filepath=path)
 
-#model = "30136.dat"
-model = "3034.dat"
-path = os.path.join(model_path, model)
-bpy.ops.import_scene.importldraw(filepath=path)
-
-deselect_all()
-bpy.data.objects["LegoGroundPlane"].select_set(True)
-bpy.ops.object.delete()
-
-brick = bpy.data.objects[f"00000_{model}"]
-scene = bpy.data.scenes['Scene']
-
-dims = brick.dimensions
-dimx, dimy, dimz = dims[0], dims[1], dims[2]
-
-#camera location
-locx, locy, locz = ((max(dimx, dimy, dimz))*1.5), ((max(dimx, dimy, dimz))*1.5), (dimz / 2)
-rotx, roty, rotz = deg_to_radians(-90), deg_to_radians(180), deg_to_radians(-45)
-
-#set camera
-bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(locx, locy, locz), rotation=(rotx, roty, rotz), scale=(1, 1, 1))
-scene.camera = bpy.context.object
-scene.render.resolution_x = 512
-scene.render.resolution_y = 512
-
-#set light
-bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(locx, locy, locz + 10), scale=(1, 1, 1))
-
-scene.cycles.device = 'GPU'
-scene.cycles.progressive = 'BRANCHED_PATH'
-scene.cycles.subsurface_samples = 5
-
-def rotate_brick(x, y, z):
     deselect_all()
-    brick.select_set(True)
-    override=bpy.context.copy()
-    override['area']=[a for a in bpy.context.screen.areas if a.type=="VIEW_3D"][0]
-    bpy.ops.transform.rotate(override, value=x, orient_axis='X')
-    bpy.ops.transform.rotate(override, value=y, orient_axis='Y')
-    bpy.ops.transform.rotate(override, value=z, orient_axis='Z')
+    bpy.data.objects["LegoGroundPlane"].select_set(True)
+    bpy.ops.object.delete()
 
-seed()
+    piece = bpy.data.objects[f"00000_{model}"]
+    scene = bpy.data.scenes['Scene']
 
-for i in range(10):
-    rotate_brick(random()*10, random()*10, random()*10)
-    bpy.context.scene.render.filepath = os.path.join(render_path, f"{model[:-4]}-{timestamp()}")
-    bpy.ops.render.render(write_still = True)
+    dims = piece.dimensions
+    dimx, dimy, dimz = dims[0], dims[1], dims[2]
+
+    #camera location
+    locx, locy, locz = ((max(dimx, dimy, dimz))+.5), ((max(dimx, dimy, dimz))+.5), (dimz / 2)
+    rotx, roty, rotz = deg_to_radians(-90), deg_to_radians(180), deg_to_radians(-45)
+
+    #set camera
+    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(locx, locy, locz), rotation=(rotx, roty, rotz), scale=(1, 1, 1))
+    scene.camera = bpy.context.object
+    scene.render.resolution_x = res
+    scene.render.resolution_y = res
+
+    #set light
+    bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(locx, locy, locz + 10), scale=(1, 1, 1))
+
+    bpy.ops.mesh.primitive_plane_add(size=50, enter_editmode=False, align='WORLD', location=(-locx, -locy, 0), scale=(1,1,1), rotation=(deg_to_radians(90), 0, deg_to_radians(-45)))
+
+    scene.cycles.device = 'GPU'
+    scene.cycles.progressive = 'BRANCHED_PATH'
+    scene.cycles.subsurface_samples = 5
+    scene.cycles.aa_samples = 64
+
+    def rotate_piece(x, y, z):
+        deselect_all()
+        piece.select_set(True)
+        override=bpy.context.copy()
+        override['area']=[a for a in bpy.context.screen.areas if a.type=="VIEW_3D"][0]
+        bpy.ops.transform.rotate(override, value=x, orient_axis='X')
+        bpy.ops.transform.rotate(override, value=y, orient_axis='Y')
+        bpy.ops.transform.rotate(override, value=z, orient_axis='Z')
+
+    for i in range(iterations):
+        rotate_piece(random_radian(), random_radian(), random_radian())
+        bpy.context.scene.render.filepath = os.path.join(outPath, f"{model[:-4]}_{i}")
+        bpy.ops.render.render(write_still = True)
+
+all_models = os.listdir("/home/spencer/ldraw/parts")
+
+for da_model in all_models:
+    folder = os.path.join("/home/spencer/Documents/GitHub/nexus/tools/renders/", f"{model[:-4]}")
+    if os.path.isdir(folder) == False:
+        os.mkdir(folder)
+    gen_pics(da_model, 32, "/home/spencer/ldraw/parts/", "/home/spencer/Documents/GitHub/nexus/tools/renders/", 256)

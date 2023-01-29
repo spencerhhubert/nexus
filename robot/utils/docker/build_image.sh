d86b0a7db06e46287227b3eea6bccf9b1112f648
spencer <spencerhhubert@gmail.com>
2023-01-29 02:18:07 -0800

    docker container works with qt gui

--- robot/utils/docker/build_image.sh
+++ robot/utils/docker/build_image.sh
@@ -1,1 +1,2 @@
+#! /bin/bash
 sudo docker build -t "nexus_ros" $PWD
--- robot/utils/docker/run_container.sh
+++ robot/utils/docker/run_container.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
-sudo docker run -d \
-    --gpus all --runtime nvidia --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -it --name "nexus_ros" "nexus_ros"
+
+CONTAINER_ID="nexus_ros"
+IMAGE_ID="nexus_ros"
+
+docker stop $CONTAINER_ID
+docker rm $CONTAINER_ID
+
+docker run -d \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --name $CONTAINER_ID \
+    -it $IMAGE_ID
+
+xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $CONTAINER_ID`
+
+#docker start $CONTAINER_ID

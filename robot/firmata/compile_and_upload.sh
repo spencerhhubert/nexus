f1a318316661be7267744c0daa2c67cd44840607
spencer <spencerhhubert@gmail.com>
2023-02-21 21:37:25 -0800

    move and delete

--- robot/firmata/compile_and_upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo arduino-cli compile --fqbn arduino:avr:mega
-#sudo arduino-cli upload --port /dev/ttyUSB1 --fqbn arduino:avr:nano
-sudo arduino-cli upload --port /dev/ttyACM0 --fqbn arduino:avr:mega
-
--- robot/run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-cd firmata/
-./compile_and_upload.sh
-cd $OLDPWD
-
-go build .
-sudo -s ./robot

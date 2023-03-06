780950280167ffeb49f3d3200fd5f6b086d747dc
spencer <spencerhhubert@gmail.com>
2023-03-05 16:46:31 -0800

    inference and auto masking

--- training/segmentation/inference.py
+++ training/segmentation/inference.py
@@ -4,7 +4,8 @@ from model import DeepLabV3SegNet
 import random
 import os
 
-model = DeepLabV3SegNet()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = DeepLabV3SegNet().to(device)
 model.load_state_dict(torch.load("models/model_1678060418.016126.pt"))
 model.eval()
 
@@ -19,9 +20,23 @@ def predictMask(img):
     img = img.float()
     img = img.unsqueeze(0)
     with torch.no_grad():
-        return model(img)
+        mask = model(img)
+    threshold = 0.5
+    mask = torch.where(mask > threshold, torch.ones_like(mask), torch.zeros_like(mask))
+    return mask
+
+def maskImage(img, mask):
+    mask = mask.squeeze(0)
+    mask = transforms.Resize((img.shape[1], img.shape[2]))(mask)
+    mask = mask.repeat(3,1,1)
+    img = torch.where(mask > 0, img, torch.zeros_like(img))
+    return img
 
 for img_name in test_images:
     img = io.read_image(os.path.join(images_path, img_name))
-    out = predictMask(img)
-    utils.save_image(out, os.path.join(output_path, img_name))
+    img = img.to(device)
+    out = predictMask(img.float())
+    masked = maskImage(img, out)
+    masked = masked.float()
+    masked = masked / 255
+    utils.save_image(masked, os.path.join(output_path, img_name))

243074730f922ccf910af6181c4ade99b3a51c59
spencer <spencerhhubert@gmail.com>
2023-04-03 16:56:05 -0700

    reference my fork of firmata with no accelsteppers

--- embedded/firmata/setup.sh
+++ embedded/firmata/setup.sh
@@ -6,6 +6,8 @@ sudo arduino-cli core install arduino:avr@1.8.3
 sudo arduino-cli lib install firmata
 sudo arduino-cli lib install servo
 sudo arduino-cli lib install --git-url https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library
-sudo arduino-cli lib install --git-url https://github.com/firmata/ConfigurableFirmata
+#sudo arduino-cli lib install --git-url https://github.com/firmata/ConfigurableFirmata
+sudo arduino-cli lib install --git-url https://github.com/spencerhhubert/ConfigurableFirmata
 sudo arduino-cli lib install ArduinoSTL
+sudo arduino-cli lib install --git-url https://github.com/waspinator/AccelStepper
 #sudo arduino-cli compile --fqbn arduino:avr:nano

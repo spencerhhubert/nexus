60f457ad6948a8e66835ff6f3f99e864f0b14b57
spencer <spencerhhubert@gmail.com>
2023-04-03 17:49:48 -0700

    creature comfort sometimes device changes when unplugging, need access in docker container

--- utils/docker/run_container.sh
+++ utils/docker/run_container.sh
@@ -11,6 +11,12 @@ docker run -d \
     -v /home/spencer/code/nexus:/nexus/ \
     --gpus all \
     --device=/dev/video0:/dev/video0 \
+    --device=/dev/video1:/dev/video1 \
+    --device=/dev/video2:/dev/video2 \
+    --device=/dev/video3:/dev/video3 \
     --device=$MC_USB:$MC_USB \
+    --device=/dev/ttyACM0:/dev/ttyACM0 \
+    --device=/dev/ttyACM1:/dev/ttyACM1 \
+    --device=/dev/ttyACM2:/dev/ttyACM2 \
     --name $CONTAINER \
     -it $IMAGE

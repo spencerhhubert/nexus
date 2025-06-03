d42e73bb111a31a25474dd0f45d3b10eb2215e49
Spencer Hubert <spencerhhubert@gmail.com>
2025-06-03 12:14:15 -0400

    dump logs to folder if --dump

--- robot/run.sh
+++ robot/run.sh
@@ -9,6 +9,21 @@ export PYTHONPATH="${PARENT_DIR}:${PYTHONPATH}"
 
 PYTHON_INTERPRETER="/opt/homebrew/opt/python@3.11/libexec/bin/python"
 
-# Run the Python script
 cd "$(dirname "$0")"
-$PYTHON_INTERPRETER main.py "$@"
+
+# Check if --dump argument is present
+if [[ " $* " == *" --dump "* ]]; then
+    # Remove --dump from arguments
+    args=()
+    for arg in "$@"; do
+        if [[ "$arg" != "--dump" ]]; then
+            args+=("$arg")
+        fi
+    done
+
+    mkdir -p ../logs
+    TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+    $PYTHON_INTERPRETER main.py "${args[@]}" &> "../logs/${TIMESTAMP}.log"
+else
+    $PYTHON_INTERPRETER main.py "$@"
+fi

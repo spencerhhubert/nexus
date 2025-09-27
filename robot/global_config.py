8312dca0f558b588fe21a5c783f9e168f79d7b14
Spencer Hubert <spencerhhubert@gmail.com>
2025-09-26 21:19:11 -0400

    config changes

--- robot/global_config.py
+++ robot/global_config.py
@@ -149,10 +149,10 @@ def buildGlobalConfig() -> GlobalConfig:
         "use_prev_bin_state": args.use_prev_bin_state,
         "main_conveyor_speed": 115,
         "feeder_conveyor_speed": 140,
-        "first_vibration_hopper_motor_speed": 160,  # first hopper that pieces enter
-        "second_vibration_hopper_motor_speed": 155,
-        "first_vibration_hopper_motor_pulse_ms": 600,
-        "second_vibration_hopper_motor_pulse_ms": 500,
+        "first_vibration_hopper_motor_speed": 162,  # first hopper that pieces enter
+        "second_vibration_hopper_motor_speed": 165,
+        "first_vibration_hopper_motor_pulse_ms": 200,
+        "second_vibration_hopper_motor_pulse_ms": 200,
         "feeder_conveyor_pulse_ms": 1000,
         "first_vibration_hopper_motor_pause_ms": 500,
         "second_vibration_hopper_motor_pause_ms": 500,
